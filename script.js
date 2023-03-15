ce893bce1bb26d5051a2aaaa95f934582585d098
Nicolás Vega Cardozo <nicolasricardovegacardozo@gmail.com>
2023-03-14 23:28:24 -0300

    third commit

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
 const productos = [
-    { nombre: "cartuchera", precio: 500 },
-    { nombre: "lapices de colores", precio: 1000 },
-    { nombre: "pegamento adhesivo", precio: 250 },
-    { nombre: "cuaderno", precio: 300 },
-    { nombre: "repuestos de hojas nº3", precio: 400 },
-    { nombre: "mochila", precio: 5000 },
-    { nombre: "marcadores", precio: 600 },
-    { nombre: "birome", precio: 100 },
+    new Producto("cartuchera", 500),
+    new Producto("lapices de colores", 1000),
+    new Producto("pegamento adhesivo", 250),
+    new Producto("cuaderno", 300),
+    new Producto("repuestos de hojas nº3", 400),
+    new Producto("mochila", 5000),
+    new Producto("marcadores", 600),
+    new Producto("birome", 100)
 ];
 
 let carrito = []
